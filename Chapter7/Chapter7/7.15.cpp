b4888f9a2efae5c9c6e1ca550d6eb83c409829b8
hwy9855 <35758582@qq.com>
2016-12-07 12:18:54 +0800

    Duplicate Elimination

---
+++ Chapter7/Chapter7/7.15.cpp
@@ -0,0 +1,29 @@
+//7.15
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[20];
+	int j = 0;
+	bool fl;
+	for (int i = 0; i < 20; i++) {
+		fl = true;
+		cin >> a[j];
+		for (int k = 0; k < j; k++) {
+			if (a[k] == a[j]) {
+				fl = false;
+				break;
+			}
+		}
+		if (fl)
+			j++;
+	}
+	for (int i = 0; i < j; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
